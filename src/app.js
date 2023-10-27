b4e390e5ae3f046a42f78a8aba14af4c16ee9d5c
Anie001 <ekpohanny@gmail.com>
2023-10-28 13:14:49 +1400

    created the first js function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
+
 let apiKey = "2e67ada581cbfe3ce3d849417cd6754c";
 let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);

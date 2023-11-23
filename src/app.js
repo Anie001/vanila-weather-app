c09859a87889aa0c847a1c8f2db936cf8dc520a7
Anie001 <ekpohanny@gmail.com>
2023-11-24 04:23:53 +1400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 function search(city) {
-  let apiKey = "2e67ada581cbfe3ce3d849417cd6754c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 function handleSubmit(event) {
@@ -10,9 +10,9 @@ function handleSubmit(event) {
 }
 
 function getForecast(city) {
-  let apiKey = "2e67ada581cbfe3ce3d849417cd6754c";
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
   //let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?query=${city}&appid=${apiKey}&units=metric`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?query=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   console.log(apiUrl);
 }
 

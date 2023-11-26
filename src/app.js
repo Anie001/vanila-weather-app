f411bfa34d20f09c77de48d16216288393577a5a
Anie001 <ekpohanny@gmail.com>
2023-11-26 20:45:11 +1400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -83,7 +83,7 @@ function displayTemperature(response) {
 }
 function search(city) {
   let apiKey = "20563feob4054a551b95t805f4a823e4";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
